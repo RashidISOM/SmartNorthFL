ca487d4fff62123cfff2eadcd2b389a007f49993
SamVonderahe <samvonderahe@gmail.com>
2020-07-01 16:40:04 -0400

    Linked database

--- TechPointProject/settings.py
+++ TechPointProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'TechPointProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '35.224.233.128',
+        'NAME': 'infoodbanks',
+        'USER': 'root',
+        'PASSWORD': 'Team1FB1845',
+
     }
 }
 
