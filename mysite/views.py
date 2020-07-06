e826b6bc0c322e09d761b1d5c389ef66fbdb8ca0
llabuzie <60725482+llabuzie@users.noreply.github.com>
2020-07-06 12:49:41 -0400

    updated views.py for templates

--- mysite/views.py
+++ mysite/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def mainpage(request):
     return render(request, 'mainpage.html', {})
+
+def inventory(request):
+    return render(request, 'inventorypage.html', {})
+
+def pantries(request):
+    return render(request, 'findpantrypage.html', {})
+
+def add(request):
+    return render(request, 'additemspage.html', {})
+
+def remove(request):
+    return render(request, 'removeitemspage.html', {})
