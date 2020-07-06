1e574e64b3ee946f7b01de73b7ead4dc3fb97433
llabuzie <60725482+llabuzie@users.noreply.github.com>
2020-07-06 12:48:52 -0400

    updated urls.py with new paths for templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,5 +6,9 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
     path('login/', LoginView.as_view(), name='login'),
+    path('inventory', views.inventory, name='inventory'),
+    path('pantries', views.pantries, name= 'pantries'),
+    path('add', views.add, name= 'add'),
+    path('remove', views.remove, name= 'remove'),
 
 ]
