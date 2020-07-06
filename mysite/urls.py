5425bad959926f41ef17537763e2b3f5411c47cd
SamVonderahe <samvonderahe@gmail.com>
2020-07-05 23:35:01 -0400

    Added login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from mysite import views
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     path('', views.mainpage, name='mainpage'),
+    path('login/', LoginView.as_view(), name='login'),
+
 ]
