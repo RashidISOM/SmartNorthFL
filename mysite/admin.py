cccab0201d0a03ee2f3aeb66d8fd37c10dd5d901
pbelpasso <63639650+pbelpasso@users.noreply.github.com>
2020-07-07 01:20:50 -0400

    Update admin.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from mysite.models import Food
-@admin.register(Food)
-class ViewAdmin(admin.ModelAdmin):
-    pass
+#from mysite.models import Food
+#@admin.register(Food)
+#class ViewAdmin(admin.ModelAdmin):
+  #  pass
 
