3a15e68d13107bf0cb04c3de3d3763c51d42e959
pbelpasso <63639650+pbelpasso@users.noreply.github.com>
2020-07-08 16:04:06 -0400

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from mysite import views
 from register import views as regView
 from django.contrib.auth.views import LoginView
+from .views import index
 
 
 
@@ -14,5 +15,5 @@ urlpatterns = [
     path('pantries', views.pantries, name= 'pantries'),
     path('add', views.add, name= 'add'),
     path('remove', views.remove, name= 'remove'),
-
+    path('index', views.index, name='index' ),
 ]
