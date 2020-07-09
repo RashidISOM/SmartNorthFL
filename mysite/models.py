4f2333808229267996011aa456fa5af4c300c36f
llabuzie <60725482+llabuzie@users.noreply.github.com>
2020-07-09 13:27:06 -0400

    updated address

--- mysite/models.py
+++ mysite/models.py
@@ -21,7 +21,13 @@ class Food(models.Model): #name of table
 
 class Pantry(models.Model):
     name = models.CharField(max_length = 100)
-    address = models.CharField(max_length=250)
+    #address = models.CharField(max_length = 250)
+    zipCode = models.CharField(max_length = 50)
+    streetAdd1 = models.CharField(max_length = 100)
+    streetAdd2 = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 100)
+    state = models.CharField(max_length = 50)
+
     #https://stackoverflow.com/questions/19130942/whats-the-best-way-to-store-phone-number-in-django-models
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
