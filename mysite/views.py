6a35a6af2a01094491b7bc4566a59a80c558ff6e
pbelpasso <63639650+pbelpasso@users.noreply.github.com>
2020-07-09 12:56:34 -0400

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -16,8 +16,12 @@ def add(request):
 def remove(request):
     return render(request, 'removeitemspage.html', {})
 
-def index(request):
-    return render(request, 'index.html', {})
+def display_food(request):
+    items = Food.objects.all()
+    context = {
+        'items' : items,
+    }
+    return render(request, 'inventorypage.html', context)
 
 ##def login(request):
   #  return render(request, 'register/login.html', {})
