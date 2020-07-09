2adeb9f08bffcc50c9ca3b51babfa886f1ff72b5
pbelpasso <63639650+pbelpasso@users.noreply.github.com>
2020-07-09 17:02:28 -0400

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -24,5 +24,30 @@ def display_food(request):
     }
     return render(request, 'inventorypage.html', context)
 
+def add_food(request):
+    if request.method == "POST":
+        form = FoodForm (request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('inventory')
+    else:
+        form = FoodForm()
+        return render(request, 'additemspage.html', {'form': form})
+
+def edit_food(request, pk):
+    item = get_object_or_404(Food, pk=pk)
+
+    if request.method == "POST":
+        form = FoodForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory')
+
+    else:
+        form = FoodForm(instance=item)
+
+        return render(request, 'edit_item.html', {'form': form})
+
 ##def login(request):
   #  return render(request, 'register/login.html', {})
