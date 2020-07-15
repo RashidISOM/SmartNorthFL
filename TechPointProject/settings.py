31eeb735fd8af9cb6469e36e132766b31d2190af
pbelpasso <63639650+pbelpasso@users.noreply.github.com>
2020-07-15 13:05:20 -0400

    Update settings.py

--- TechPointProject/settings.py
+++ TechPointProject/settings.py
@@ -132,3 +132,11 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/inventory'
 
 LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'Techpointteam1nonprofit@gmail.com'
+EMAIL_HOST_PASSWORD = 'Thx4TheXP178'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
