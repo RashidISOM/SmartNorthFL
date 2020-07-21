7cb87f2a794b9e5ed6747ac2868eac0a0a514f7a
llabuzie <60725482+llabuzie@users.noreply.github.com>
2020-07-20 23:40:03 -0400

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
+from datetime import datetime, date, time
 # Create your models here.
 
 
