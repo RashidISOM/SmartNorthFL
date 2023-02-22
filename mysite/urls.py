a5a230fb0772f004cc5532710e641de3e4ec2c00
RashidISOM <124409349+RashidISOM@users.noreply.github.com>
2023-02-21 21:24:32 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, re_path
-from django.urls.re_path import include, re_path
 from mysite import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
