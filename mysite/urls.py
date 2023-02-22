4a4aeb07e88019bcab17f96a0acca57771a5a85e
RashidISOM <124409349+RashidISOM@users.noreply.github.com>
2023-02-21 21:12:49 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import include, re_path
 from mysite import views
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
@@ -16,18 +16,18 @@ urlpatterns = [
     path('add/', views.add, name= 'add'),
     path('remove/', views.remove, name= 'remove'),
     path('about/', views.about, name='about'),
-    url (r'^add_pantry$', add_pantry, name = 'add_pantry'),
-    url (r'^edit_pantry/$', edit_pantry, name = 'edit_pantry'),
-    url (r'^display_food$', display_food, name = 'display_food'),
-    url (r'^add_food$', add_food, name = 'add_food'),
-    url (r'^edit_food/(?P<pk>\d+)$', edit_food, name = 'edit_food'),
-    url (r'^delete_food/(?P<pk>\d+)$', delete_food, name = 'delete_food'),
-    url (r'^send_mail_form$', send_mail_form, name = 'send_mail_form'),
-    url (r'^sign_up_form$', sign_up_form, name = 'sign_up_form'),
-    url (r'^pantry_inventory/(?P<pantry_id>[0-9]+)$', pantry_inventory, name = 'pantry_inventory'),
-    url (r'^add_hours$', add_hours, name = 'add_hours'),
-    url (r'^edit_hours/(?P<pk>\d+)$', edit_hours, name = 'edit_hours'),
-    url (r'^delete_hours/(?P<pk>\d+)$', delete_hours, name = 'delete_hours'),
-    url (r'^add_need$', add_need, name = 'add_need'),
-    url (r'^delete_need/(?P<pk>\d+)$', delete_need, name = 'delete_need'),
+    re_path (r'^add_pantry$', add_pantry, name = 'add_pantry'),
+    re_path (r'^edit_pantry/$', edit_pantry, name = 'edit_pantry'),
+    re_path (r'^display_food$', display_food, name = 'display_food'),
+    re_path (r'^add_food$', add_food, name = 'add_food'),
+    re_path (r'^edit_food/(?P<pk>\d+)$', edit_food, name = 'edit_food'),
+    re_path (r'^delete_food/(?P<pk>\d+)$', delete_food, name = 'delete_food'),
+    re_path (r'^send_mail_form$', send_mail_form, name = 'send_mail_form'),
+    re_path (r'^sign_up_form$', sign_up_form, name = 'sign_up_form'),
+    re_path (r'^pantry_inventory/(?P<pantry_id>[0-9]+)$', pantry_inventory, name = 'pantry_inventory'),
+    re_path (r'^add_hours$', add_hours, name = 'add_hours'),
+    re_path (r'^edit_hours/(?P<pk>\d+)$', edit_hours, name = 'edit_hours'),
+    re_path (r'^delete_hours/(?P<pk>\d+)$', delete_hours, name = 'delete_hours'),
+    re_path (r'^add_need$', add_need, name = 'add_need'),
+    re_path (r'^delete_need/(?P<pk>\d+)$', delete_need, name = 'delete_need'),
 ]
